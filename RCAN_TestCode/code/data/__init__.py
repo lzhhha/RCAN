ab3e8a1e8380fbc6611dce72f851a804917a4d6a
lzhhha <46672763+lzhhha@users.noreply.github.com>
2020-11-05 20:02:58 +0800

    修改

--- RCAN_TestCode/code/data/__init__.py
+++ RCAN_TestCode/code/data/__init__.py
@@ -25,7 +25,10 @@ class Data:
                 **kwargs
             )
 
+        print(args.data_test)
+        args.data_test = 'Set5'
         if args.data_test in ['Set5', 'Set14', 'B100', 'Urban100']:
+
             if not args.benchmark_noise:
                 module_test = import_module('data.benchmark')
                 testset = getattr(module_test, 'Benchmark')(args, train=False)
@@ -37,6 +40,8 @@ class Data:
                 )
 
         else:
+            print('\n')
+            print('liuzhe======================')
             module_test = import_module('data.' +  args.data_test.lower())
             testset = getattr(module_test, args.data_test)(args, train=False)
 
